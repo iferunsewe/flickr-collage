ed2e752ac8b3cf38720d09da4327c12ff3c2e807
iferunsewe <iferunsewe@gmail.com>
2016-02-29 21:37:21 +0000

    Taking arguments from the command line

--- lib/search.rb
+++ lib/search.rb
@@ -1,5 +1,27 @@
-require "search_flickr/version"
+require "./lib/search_flickr/version"
 
 module SearchFlickr
-  
+  extend self
+
+  def create_keywords_array
+    keywords_array = []
+    ARGV.each do|a|
+      keywords_array << a
+    end
+    keywords_array
+  end
+
+  class FlickrApi
+    def initialize(keywords)
+      @keywords = keywords
+    end
+
+    def get_flickr_results
+      @keywords.map do |keyword|
+        
+      end
+    end
+  end
 end
+
+SearchFlickr.create_keywords_array
