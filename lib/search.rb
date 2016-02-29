2b1eb572497e8014751a1aa0de0938ddb10ff95a
iferunsewe <iferunsewe@gmail.com>
2016-02-29 21:57:13 +0000

    Added flickraw gem and got api keys and secret

--- lib/search.rb
+++ lib/search.rb
@@ -1,7 +1,10 @@
 require "./lib/search_flickr/version"
+require 'flickraw'
 
 module SearchFlickr
   extend self
+  FlickRaw.api_key="920490d1b6ca2176925a316eb1ed4861"
+  FlickRaw.shared_secret="3b152563356e4da4"
 
   def create_keywords_array
     keywords_array = []
@@ -18,10 +21,12 @@ module SearchFlickr
 
     def get_flickr_results
       @keywords.map do |keyword|
-        
+
       end
     end
   end
 end
 
 SearchFlickr.create_keywords_array
+
+# https://api.flickr.com/services - endpoint
